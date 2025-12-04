f2879172c2b69140981eacf19c60ff1c7560d611
privatecore <privatecorepp@gmail.com>
2025-12-04 11:09:15 +0100

    Implement ItemTemplate struct and ObjectGuid value type class for future use

--- src/Bindings/Exports/ItemExports.cpp
+++ src/Bindings/Exports/ItemExports.cpp
@@ -4,10 +4,39 @@
 namespace bp = boost::python;
 
 /**
+ * @brief ItemTemplate struct containing static item database data
+ */
+void export_item_template_struct()
+{
+    bp::class_<ItemTemplate> item_template_struct("ItemTemplate", bp::no_init);
+
+    item_template_struct.def_readonly("Id", &ItemTemplate::ItemId);
+    item_template_struct.def_readonly("Class", &ItemTemplate::Class);
+    item_template_struct.def_readonly("SubClass", &ItemTemplate::SubClass);
+    item_template_struct.def_readonly("Name", &ItemTemplate::Name1);
+    item_template_struct.def_readonly("Quality", &ItemTemplate::Quality);
+    item_template_struct.def_readonly("BuyCount", &ItemTemplate::BuyCount);
+    item_template_struct.def_readonly("BuyPrice", &ItemTemplate::BuyPrice);
+    item_template_struct.def_readonly("SellPrice", &ItemTemplate::SellPrice);
+    item_template_struct.def_readonly("InventoryType", &ItemTemplate::InventoryType);
+    item_template_struct.def_readonly("AllowableClass", &ItemTemplate::AllowableClass);
+    item_template_struct.def_readonly("AllowableRace", &ItemTemplate::AllowableRace);
+    item_template_struct.def_readonly("ItemLevel", &ItemTemplate::ItemLevel);
+    item_template_struct.def_readonly("MaxCount", &ItemTemplate::MaxCount);
+    item_template_struct.def_readonly("Stackable", &ItemTemplate::Stackable);
+}
+
+/**
  * @brief Item Class - Represents game items (armor, weapons, consumables, etc.)
  */
 void export_item_class()
 {
-    bp::class_<Item, boost::noncopyable>("Item", bp::no_init)
-        .def("GetEntry", &Item::GetEntry);
+    bp::class_<Item, boost::noncopyable> item_class("Item", bp::no_init);
+
+    item_class.def("GetTemplate", &Item::GetTemplate, bp::return_internal_reference<>());
+
+    item_class.def("GetEntry", &Item::GetEntry);
+    item_class.def("GetCount", &Item::GetCount);
+    item_class.def("GetSlot", &Item::GetSlot);
+    item_class.def("GetBagSlot", &Item::GetBagSlot);
 }
---
+++ src/Bindings/Exports/ObjectGuidExport.cpp
@@ -0,0 +1,38 @@
+// ObjectGuidExport.cpp
+#include "PythonHeaders.h"
+#include "ObjectGuid.h"
+
+namespace bp = boost::python;
+
+/**
+ * @brief ObjectGuid Class - Unique identifier value type for game entities
+ */
+void export_objectguid_class()
+{
+    bp::class_<ObjectGuid> objectguid_class("ObjectGuid");
+
+    objectguid_class.def(bp::init<>());
+    objectguid_class.def(bp::init<uint64>(bp::arg("rawValue")));
+
+    objectguid_class.def("GetRawValue", &ObjectGuid::GetRawValue);
+    objectguid_class.def("GetCounter", &ObjectGuid::GetCounter);
+    objectguid_class.def("IsEmpty", &ObjectGuid::IsEmpty);
+    objectguid_class.def("Clear", &ObjectGuid::Clear);
+
+    objectguid_class.def("IsPlayer", &ObjectGuid::IsPlayer);
+    objectguid_class.def("IsCreature", &ObjectGuid::IsCreature);
+    objectguid_class.def("IsGameObject", &ObjectGuid::IsGameObject);
+    objectguid_class.def("IsItem", &ObjectGuid::IsItem);
+    objectguid_class.def("IsPet", &ObjectGuid::IsPet);
+    objectguid_class.def("IsVehicle", &ObjectGuid::IsVehicle);
+    objectguid_class.def("IsUnit", &ObjectGuid::IsUnit);
+
+    objectguid_class.def(bp::self == bp::self);
+    objectguid_class.def(bp::self != bp::self);
+    objectguid_class.def(bp::self < bp::self);
+
+    objectguid_class.def("ToString", &ObjectGuid::ToString);
+    objectguid_class.def("__str__", &ObjectGuid::ToString);
+    objectguid_class.def("__repr__", &ObjectGuid::ToString);
+    objectguid_class.def("__hash__", &ObjectGuid::GetRawValue);
+}
